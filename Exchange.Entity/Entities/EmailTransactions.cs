d73e4cc3a4076fe6b4df37b1d8cbd8ea9f1080be
Tugrul <Tugrul@DESKTOP-D80M3PV>
2023-06-08 14:51:39 +0300

    SomeModification is Done

---
+++ Exchange.Entity/Entities/EmailTransactions.cs
@@ -0,0 +1,18 @@
+﻿using Exchange.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange.Entity.Entities
+{
+    public class EmailTransactions:EntityBase
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public AppUser User { get; set; }
+        public Guid UserID { get; set; }
+        
+    }
+}
